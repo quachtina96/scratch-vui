97b8918a5ab2d26e40906c2d32463f2870c8d027
Tina Quach <quacht@mit.edu>
2018-10-30 11:39:57 -0400

    on space, stop speech synthesis

--- src/prototype.js
+++ src/prototype.js
@@ -165,6 +165,7 @@ if (!('webkitSpeechRecognition' in window)) {
         recognition.stop();
         return;
       } else {
+        scratch.pm.synth.cancel();
         recognition.start();
       }
     }
