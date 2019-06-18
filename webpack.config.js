a9f93d0cbef5281fca91f159ea98b76f54ea4b7a
Tina Quach <quacht@mit.edu>
2019-06-18 13:26:41 -0400

    bind to whatever port

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,22 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/prototype.js',
-    output: {
-        path: path.resolve(__dirname, './build'),
-        filename: 'bundle.js'
-    },
-    plugins: [
-        new CopyWebpackPlugin([{
-            from: 'static',
-            to: '.'
-        }])
-    ],
-    watch: true
+	mode: 'development',
+	entry: './src/prototype.js',
+	output: {
+		path: path.resolve(__dirname, './build'),
+		filename: 'bundle.js'
+	},
+	plugins: [
+		new CopyWebpackPlugin([{
+			from: 'static',
+			to: '.'
+		}])
+	],
+	watch: true,
+  devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		compress: true,
+		port: process.env.PORT || 1337
+  }
 };
